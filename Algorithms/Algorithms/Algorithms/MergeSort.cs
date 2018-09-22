012144650a0878b89ab7486599d9204c2f85a898
Nicholas Maselli <maselli.nicholas@gmail.com>
2018-09-22 00:13:24 -0400

    Adding website design and Sorting Algorithm side tab

---
+++ Algorithms/Algorithms/Algorithms/MergeSort.cs
@@ -0,0 +1,75 @@
+﻿using system;
+
+namespace Algorithms
+{
+	public class MergeSort
+	{
+		public static void MergeSort(int[] array) {
+			int low = 0;
+			int high = array.Length - 1;
+						
+			MergeSort(array, low, high, middle);
+		}
+		
+		private static void MergeSort(int array[], int low, int high){
+			if (low < high) {
+				int middle = low + (high-low)/2;
+				
+				MergeSort(array, low, middle);
+				MergeSort(array, middle + 1, high);
+				
+				merge(array, low, high, middle);
+			}			
+		}
+		
+		private static void merge(int[] array, int low, int high, int middle) {
+			int leftSize = middle - low + 1;
+			int rightSize = high - middle;
+			
+			int[] leftArray = new int[leftSize];
+			int[] rightArray = new int[rightSize];
+			
+			//Place data in these temporary array
+			for (int i = 0; i < leftSize; i++) {
+				leftArray[i] = array[low + i];
+			}
+			
+			for (int j = 0; j < rightSize; j++) {
+				rightArray[j] = array[middle + 1 + j];
+			}
+			
+			int i = 0;
+			int j = 0;
+			
+			int k = low;
+			while (i < leftSize && j < rightSize) {
+				if (leftArray[i] <= rightArray[j]) {
+					array[k] = leftArray[i];
+					i++;
+				}
+				else {
+					array[k] = rightArray[j];
+					j++
+				}
+				k++;
+			}
+			
+			//Add the remaining elements to the input array
+			while(i < leftSize) {
+				array[k] = leftArray[i];
+				i++;
+				k++;
+			}
+			
+			while(j < rightSize) {
+				array[k] = rightArray[j];
+				j++;
+				k++;
+			}			
+		}
+	
+		public static void Main(string[] args) {
+			
+		}
+	}
+}
---
+++ Algorithms/Algorithms/Algorithms/QuickSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class QuickSort
+    {
+		/*QuickSort is an O(NLog(N)) divide and conquer sorting algorithm that performs and average of Log(N)
+		steps of its partition function which is O(N). Need in place and stable here*/		
+		public static void QuickSort(int[] array) {
+			int low = 0;
+			int high = array.Length - 1;
+			QuickSort(array, low, high);
+		}
+		
+		private static void QuickSort(int[] array, int low, int high) {
+			if (low < high) {
+				int pivot = partition(array, low, high);				
+				QuickSort(array, low, pivot-1);
+				QuickSort(array, pivot, high);
+			}
+		}
+		
+		/*Key will be the high value for this partition function. Partition is a
+		1 pass linear O(N) time algorithm*/
+		private static int partition(int[] array, int low, int high) {
+		
+			//Ensure low and high are within bounds
+			/*
+			if (low < 0 || high >= array.Length) {
+				return 0;
+			}
+			*/
+			
+			int temp;
+			int pivot = high;
+			while (low <= pivot) {
+				
+				//Increment low if the low index value is greater than the pivot index value
+				//It is already correctly partitioned. Swap and decrement the pivot otherwise.
+				if (array[low] <= array[pivot]) {
+					low++;
+				}
+				else {
+					temp = array[low];
+					array[low] = array[pivot];
+					array[pivot] = temp;
+					
+					pivot--;
+				}				
+			}
+			
+			return pivot;
+		}
+	
+		public static void Main(string[] args) {			
+			int[] sort = {2, 5, 8, 3, 8, 34, 4, 45, 6, 45, 23, 45, 6,23 4, 2, 2, 44};
+			QuickSort(sort);
+		}
+    }
+}
