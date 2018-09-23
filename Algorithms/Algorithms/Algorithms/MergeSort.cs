925329acfc8cc8ed41c86531386b214ecaff377b
Nicholas Maselli <maselli.nicholas@gmail.com>
2018-09-22 21:36:43 -0400

    Finalizing formatting and updating sorting algorithms after running on local machine

--- Algorithms/Algorithms/Algorithms/MergeSort.cs
+++ Algorithms/Algorithms/Algorithms/MergeSort.cs
@@ -1,75 +1,103 @@
-﻿using system;
+﻿using System;
 
 namespace Algorithms
 {
-	public class MergeSort
-	{
-		public static void MergeSort(int[] array) {
-			int low = 0;
-			int high = array.Length - 1;
-						
-			MergeSort(array, low, high, middle);
-		}
-		
-		private static void MergeSort(int array[], int low, int high){
-			if (low < high) {
-				int middle = low + (high-low)/2;
-				
-				MergeSort(array, low, middle);
-				MergeSort(array, middle + 1, high);
-				
-				merge(array, low, high, middle);
-			}			
-		}
-		
-		private static void merge(int[] array, int low, int high, int middle) {
-			int leftSize = middle - low + 1;
-			int rightSize = high - middle;
-			
-			int[] leftArray = new int[leftSize];
-			int[] rightArray = new int[rightSize];
-			
-			//Place data in these temporary array
-			for (int i = 0; i < leftSize; i++) {
-				leftArray[i] = array[low + i];
-			}
-			
-			for (int j = 0; j < rightSize; j++) {
-				rightArray[j] = array[middle + 1 + j];
-			}
-			
-			int i = 0;
-			int j = 0;
-			
-			int k = low;
-			while (i < leftSize && j < rightSize) {
-				if (leftArray[i] <= rightArray[j]) {
-					array[k] = leftArray[i];
-					i++;
-				}
-				else {
-					array[k] = rightArray[j];
-					j++
-				}
-				k++;
-			}
-			
-			//Add the remaining elements to the input array
-			while(i < leftSize) {
-				array[k] = leftArray[i];
-				i++;
-				k++;
-			}
-			
-			while(j < rightSize) {
-				array[k] = rightArray[j];
-				j++;
-				k++;
-			}			
-		}
-	
-		public static void Main(string[] args) {
-			
-		}
-	}
+    public class MergeSort
+    {
+        public static void mergesort(int[] array)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+
+            mergesort(array, low, high);
+        }
+
+        private static void mergesort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int middle = low + (high - low) / 2;
+
+                mergesort(array, low, middle);
+                mergesort(array, middle + 1, high);
+
+                merge(array, low, high, middle);
+            }
+        }
+
+        private static void merge(int[] array, int low, int high, int middle)
+        {
+            int leftSize = middle - low + 1;
+            int rightSize = high - middle;
+
+            int[] leftArray = new int[leftSize];
+            int[] rightArray = new int[rightSize];
+
+            //Place data in these temporary arrays
+            for (int index = 0; index < leftSize; index++)
+            {
+                leftArray[index] = array[low + index];
+            }
+
+            for (int index = 0; index < rightSize; index++)
+            {
+                rightArray[index] = array[middle + 1 + index];
+            }
+
+            int i = 0;
+            int j = 0;
+
+            int k = low;
+            while (i < leftSize && j < rightSize)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+
+            //Add the remaining elements to the input array
+            while (i < leftSize)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < rightSize)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+
+        public static void printArray(int[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+            Console.Write(array[array.Length - 1]);
+            Console.Write("]");
+
+            //Console.Read() holds console open until closed
+            Console.Read();
+        }
+        
+        public static void Main(string[] args)
+        {
+            int[] sort = { 2, 5, 8, 3, 8, 34, 4, 45, 6, 45, 23, 45, 6, 23, 4, 2, 2, 44 };
+            mergesort(sort);
+            printArray(sort);
+        }
+    }
 }
